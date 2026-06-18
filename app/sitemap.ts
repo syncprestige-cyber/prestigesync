cb5bf698adac8908fa7c5ef919affe8520eabe9e
clipperlabai <clipperlabai@gmail.com>
2026-06-18 11:36:12 +0700

    Simplify sitemap for Google

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://prestigesync.vercel.app',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'daily',
       priority: 1,
     },
   ];
