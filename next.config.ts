9a550bff936fb367a4cf02bcc8e73828d0503a35
clipperlabai <clipperlabai@gmail.com>
2026-06-19 15:01:41 +0700

    fix image proportions, local logo, next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.imgur.com" },
+    ],
+  },
+  allowedDevOrigins: ["192.168.100.74"],
 };
 
 export default nextConfig;
