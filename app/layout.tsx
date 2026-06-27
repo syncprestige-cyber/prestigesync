9233d16164d16ab1ba34a6abbf27eef5ce47e654
clipperlabai <syncprestige@gmail.com>
2026-06-27 13:18:12 +0700

    Add Google AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -39,6 +40,14 @@ export default function RootLayout({
         gtag('js', new Date());
         gtag('config', 'G-Z255FJE85S');
         `}} />
+
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3213485046839115"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
